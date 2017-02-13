684f8db10d80e99b3142bc291a57153f93fe3d3f
Mi3girb <zluka.888@gmail.com>
2017-02-13 17:27:39 +0300

    fix upload_file_size value

--- admin/controller/common/filemanager.php
+++ admin/controller/common/filemanager.php
@@ -461,7 +461,7 @@ class ControllerCommonFileManager extends Controller {
                 }
 
                 if ($this->request->files['image']['size'] > $this->ocstore->getUploadMaxFileSizeBytes()) {
-                    $json['error'] = sprintf($this->language->get('error_file_size'), ini_get('upload_max_filesize'));
+                    $json['error'] = sprintf($this->language->get('error_file_size'), $this->ocstore->getUploadMaxFileSizeBytes());
                 }
 
                 // Allowed file extension type
@@ -481,7 +481,7 @@ class ControllerCommonFileManager extends Controller {
                     $json['error'] = 'error_upload_' . $this->request->files['image']['error'];
                 }
             } else {
-                $json['error'] = sprintf($this->language->get('error_file'), ini_get('upload_max_filesize'));
+                $json['error'] = sprintf($this->language->get('error_file'), $this->ocstore->getUploadMaxFileSizeBytes());
             }
         } else {
             $json['error'] = $this->language->get('error_directory');
